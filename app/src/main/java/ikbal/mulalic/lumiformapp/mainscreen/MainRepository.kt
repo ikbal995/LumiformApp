60b1d4a47305eacc51292cfc7de751367685509e
ikiMulalic123 <62341655+ikiMulalic123@users.noreply.github.com>
2025-07-17 23:31:08 +0200

    Unused imports and classes deleted

--- app/src/main/java/ikbal/mulalic/lumiformapp/mainscreen/MainRepository.kt
+++ app/src/main/java/ikbal/mulalic/lumiformapp/mainscreen/MainRepository.kt
@@ -1,7 +1,6 @@
 package ikbal.mulalic.lumiformapp.mainscreen
 
 import ikbal.mulalic.data.local.entity.BaseEntity
-import ikbal.mulalic.data.local.entity.PageEntity
 import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
--- data/src/main/java/ikbal/mulalic/data/remote/RetrofitModule.kt
+++ data/src/main/java/ikbal/mulalic/data/remote/RetrofitModule.kt
@@ -1,17 +1,11 @@
 package ikbal.mulalic.data.remote
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import ikbal.mulalic.data.LumiType
-import ikbal.mulalic.data.remote.dto.BaseDto
-import ikbal.mulalic.data.remote.dto.PageDto
-import ikbal.mulalic.data.remote.dto.QuestionDto
-import ikbal.mulalic.data.remote.dto.SectionDto
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
